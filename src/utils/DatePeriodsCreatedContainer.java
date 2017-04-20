a9e705862edd983f9aaf050a99e49149bb9416b0
mtumilowicz <michal.tumilowicz01@gmail.com>
2017-04-20 18:08:03 +0200

    [poligon] DatePeriodsCreatedContainer.java refactoring

--- src/utils/DatePeriodsCreatedContainer.java
+++ src/utils/DatePeriodsCreatedContainer.java
@@ -11,17 +11,14 @@ public class DatePeriodsCreatedContainer {
     
     public static DatePeriod period_1_2 = DatePeriod.DatePeriodFactory.get(new Date(1), new Date(2));
     public static DatePeriod period_1_3 = DatePeriod.DatePeriodFactory.get(new Date(1), new Date(3));
+    public static DatePeriod period_1_4 = DatePeriod.DatePeriodFactory.get(new Date(1), new Date(4));
 
     public static DatePeriod period_2_3 = DatePeriod.DatePeriodFactory.get(new Date(2), new Date(3));
     public static DatePeriod period_2_4 = DatePeriod.DatePeriodFactory.get(new Date(2), new Date(4));
     
     public static DatePeriod period_3_4 = DatePeriod.DatePeriodFactory.get(new Date(3), new Date(4));
     
-    public static DatePeriod period_1_4 = DatePeriod.DatePeriodFactory.get(new Date(1), new Date(4));
-    
     public static DatePeriod period_1_oo = DatePeriod.DatePeriodFactory.get(new Date(1), null);
-
     public static DatePeriod period_2_oo = DatePeriod.DatePeriodFactory.get(new Date(2), null);
-
     public static DatePeriod period_3_oo = DatePeriod.DatePeriodFactory.get(new Date(3), null);
 }
