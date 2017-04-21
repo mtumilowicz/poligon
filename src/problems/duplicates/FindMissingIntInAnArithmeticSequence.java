2ced8a3e72f5065b276a8190e31ae85526e1aad0
mtumilowicz <michal.tumilowicz01@gmail.com>
2017-04-21 14:44:35 +0200

    [poligon] duplicates + tests

--- src/problems/duplicates/FindMissingIntInAnArithmeticSequence.java
+++ src/problems/duplicates/FindMissingIntInAnArithmeticSequence.java
@@ -5,15 +5,15 @@ import com.google.common.base.Preconditions;
 /**
  * Created by mtumilowicz on 2017-04-21.
  */
-public class FindMissingIntInAnArrayOfConsecutiveIntegersFromOne {
-    public static int find(int[] array) {
+public class FindMissingIntInAnArithmeticSequence {
+    public static int find(int[] array, int base, int diff) {
         Preconditions.checkArgument(array != null);
         Preconditions.checkArgument(array.length > 1);
         int first = array[0];
-        int xorAll = 1;
+        int xorAll = base;
         int xorArray = first;
-        for (int i = 2; i <= array.length+1; i++) {
-            xorAll^=i;
+        for (int i = 1; i <= array.length; i++) {
+            xorAll^=(base+i*diff);
         }
         for (int i = 1; i < array.length; i++) {
             xorArray^=array[i];
