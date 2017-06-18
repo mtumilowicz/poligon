d4a1c1edd5440d3bca00794562a30220d50a0e1c
mtumilowicz <michal.tumilowicz01@gmail.com>
2017-06-18 10:44:47 +0200

    [enums] cache

--- src/problems/enums/OrderStatus.java
+++ src/problems/enums/OrderStatus.java
@@ -3,6 +3,8 @@ package problems.enums;
 import com.google.common.collect.ImmutableSet;
 
 import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
 
 /**
  * Created by mtumilowicz on 2017-06-17.
@@ -11,38 +13,47 @@ public enum OrderStatus {
     
     NEW {
         @Override
-        public ImmutableSet<OrderStatus> getNextStatuses() {
-            return ImmutableSet.copyOf(Arrays.asList(IN_PROGRESS, REJECTED));
+        public void putNextStatusesInCache() {
+            nextStatusesCache.put(this, ImmutableSet.copyOf(Arrays.asList(IN_PROGRESS, REJECTED)));
         }
     },
     IN_PROGRESS {
         @Override
-        public ImmutableSet<OrderStatus> getNextStatuses() {
-            return ImmutableSet.copyOf(Arrays.asList(FILLED, REJECTED));
+        public void putNextStatusesInCache() {
+            nextStatusesCache.put(this, ImmutableSet.copyOf(Arrays.asList(FILLED, REJECTED)));
         }
     },
     FILLED {
         @Override
-        public ImmutableSet<OrderStatus> getNextStatuses() {
-            return ImmutableSet.copyOf(Arrays.asList(CLOSED));
+        public void putNextStatusesInCache() {
+            nextStatusesCache.put(this, ImmutableSet.copyOf(Arrays.asList(CLOSED)));
         }
     },
     CLOSED {
         @Override
-        public ImmutableSet<OrderStatus> getNextStatuses() {
-            return ImmutableSet.of();
+        public void putNextStatusesInCache() {
+            nextStatusesCache.put(this, ImmutableSet.of());
         }
     },
     REJECTED {
         @Override
-        public ImmutableSet<OrderStatus> getNextStatuses() {
-            return ImmutableSet.of();
+        public void putNextStatusesInCache() {
+            nextStatusesCache.put(this, ImmutableSet.of());
         }
     };
     
-    public abstract ImmutableSet<OrderStatus> getNextStatuses();
+    public static final Map<OrderStatus, ImmutableSet<OrderStatus>> nextStatusesCache = new EnumMap<>(OrderStatus.class);
+    
+    public ImmutableSet<OrderStatus> getNextStatuses() {
+        if (nextStatusesCache.get(this) == null) {
+            putNextStatusesInCache();
+        }
+        return nextStatusesCache.get(this);
+    }
+    
+    public abstract void putNextStatusesInCache();
     
     public boolean isPossible(OrderStatus status) {
-        return getNextStatuses().contains(status);
+        return nextStatusesCache.get(this).contains(status);
     }
 }
