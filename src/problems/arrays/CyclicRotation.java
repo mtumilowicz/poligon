7f09657fd9c20b6e315d3f87e95f25fe2dee56ab
mtumilowicz <michal.tumilowicz01@gmail.com>
2017-07-04 20:24:58 +0200

    [arrays] usage of Collections.rotate

--- src/problems/arrays/CyclicRotation.java
+++ src/problems/arrays/CyclicRotation.java
@@ -3,42 +3,22 @@ package problems.arrays;
 import com.google.common.base.Preconditions;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by mtumilowicz on 2017-05-04.
  */
 public class CyclicRotation {
-    public static int[] rotate(int[] a, int order) {
+    public static Integer[] rotate(Integer[] a, int order) {
         Preconditions.checkArgument(a != null);
         Preconditions.checkArgument(a.length > 0);
         Preconditions.checkArgument(order >= 0);
 
-        int[] copy = Arrays.copyOf(a, a.length);
-        if(order > copy.length){
-            order = order % copy.length;
-        }
-        
-        int split = copy.length - order;
+        List<Integer> integers = Arrays.asList(a);
 
-        int[] reversedFirstPart = reverse(copy, 0, split - 1);
-        int[] reversedFirstAndSecondPart = reverse(reversedFirstPart, split, copy.length - 1);
-        return reverse(reversedFirstAndSecondPart, 0, copy.length - 1);
-    }
-
-    private static int[] reverse(int[] a, int left, int right) {
-        Preconditions.checkArgument(a != null && a.length > 0);
-        Preconditions.checkArgument(left >= 0);
-        Preconditions.checkArgument(right <= a.length);
+        Collections.rotate(integers, order);
         
-        int[] copy = Arrays.copyOf(a, a.length);
-        
-        while(left < right) {
-            int temp = copy[left];
-            copy[left] = copy[right];
-            copy[right] = temp;
-            left++;
-            right--;
-        }
-        return copy;
+        return integers.toArray(new Integer[a.length]);
     }
 }
--- src/test/arrays/CyclicRotationTests.java
+++ src/test/arrays/CyclicRotationTests.java
@@ -18,42 +18,42 @@ public class CyclicRotationTests {
 
     @Test(expected = IllegalArgumentException.class)
     public void emptyArray() {
-        CyclicRotation.rotate(new int[]{}, 1);
+        CyclicRotation.rotate(new Integer[]{}, 1);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void nonPositiveOrder() {
-        CyclicRotation.rotate(new int[]{1}, -1);
+        CyclicRotation.rotate(new Integer[]{1}, -1);
     }
 
     @Test
     public void order0() {
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, CyclicRotation.rotate(new int[]{1, 2, 3, 4, 5}, 0));
+        assertArrayEquals(new Integer[]{1, 2, 3, 4, 5}, CyclicRotation.rotate(new Integer[]{1, 2, 3, 4, 5}, 0));
     }
 
     @Test
     public void order0Neq() {
-        assertNotEquals(new int[]{2, 1, 3, 4, 5}, CyclicRotation.rotate(new int[]{1, 2, 3, 4, 5}, 0));
+        assertNotEquals(new Integer[]{2, 1, 3, 4, 5}, CyclicRotation.rotate(new Integer[]{1, 2, 3, 4, 5}, 0));
     }
 
     @Test
     public void len2ord2Eq() {
-        assertArrayEquals(new int[]{2, 1}, CyclicRotation.rotate(new int[]{1,2}, 1));
-        CyclicRotation.rotate(new int[]{1,2}, 1);
+        assertArrayEquals(new Integer[]{2, 1}, CyclicRotation.rotate(new Integer[]{1,2}, 1));
+        CyclicRotation.rotate(new Integer[]{1,2}, 1);
     }
 
     @Test
     public void len2ord2Neq() {
-        assertNotEquals(new int[]{1, 2}, CyclicRotation.rotate(new int[]{1,2}, 1));
+        assertNotEquals(new Integer[]{1, 2}, CyclicRotation.rotate(new Integer[]{1,2}, 1));
     }
 
     @Test
     public void len6ord4Eq() {
-        assertArrayEquals(new int[]{3, 4, 5, 6, 1, 2}, CyclicRotation.rotate(new int[]{1, 2, 3, 4, 5, 6}, 4));
+        assertArrayEquals(new Integer[]{3, 4, 5, 6, 1, 2}, CyclicRotation.rotate(new Integer[]{1, 2, 3, 4, 5, 6}, 4));
     }
 
     @Test
     public void len6ord4Neq() {
-        assertNotEquals(new int[]{3, 1, 5, 6, 4, 2}, CyclicRotation.rotate(new int[]{1, 2, 3, 4, 5, 6}, 4));
+        assertNotEquals(new Integer[]{3, 1, 5, 6, 4, 2}, CyclicRotation.rotate(new Integer[]{1, 2, 3, 4, 5, 6}, 4));
     }
 }
