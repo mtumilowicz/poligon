bd5c18118ca6085342e00d40d0c2bba42de06f53
mtumilowicz <michal.tumilowicz01@gmail.com>
2017-09-13 19:02:27 +0200

    string rotation check, tests

---
+++ src/problems/strings/CheckIfStringIsCyclicRotationOfOther.java
@@ -0,0 +1,23 @@
+package problems.strings;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by mtumilowicz on 2017-09-13.
+ */
+public class CheckIfStringIsCyclicRotationOfOther {
+    public static boolean check(String first, String second) {
+        if (first == null && second != null || first != null && second == null) {
+            return false;
+        }
+        first = StringUtils.defaultIfEmpty(first, "");
+        second = StringUtils.defaultIfEmpty(second, "");
+        return StringUtils.contains(
+                StringUtils.join(first, first),
+                second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(check("a", null));
+    }
+}
