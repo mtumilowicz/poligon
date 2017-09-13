6e6079dd399480ad85dba19fe3c38821ea4c90e7
mtumilowicz <michal.tumilowicz01@gmail.com>
2017-09-13 21:36:06 +0200

    refactoring

--- src/problems/primes/Sieve.java
+++ src/problems/primes/Sieve.java
@@ -11,16 +11,16 @@ public class Sieve {
     
     public static boolean[] boolSieve(int upperBound) {
         Preconditions.checkArgument(upperBound > 1);
-        boolean[] isPrime = new boolean[upperBound + 1];
-        Arrays.fill(isPrime, true);
-        isPrime[0]=false;
-        isPrime[1]=false;
+        boolean[] sieve = new boolean[upperBound + 1];
+        Arrays.fill(sieve, true);
+        sieve[0]=false;
+        sieve[1]=false;
         for (int m = 2; m <= (int) Math.sqrt(upperBound); m++) {
-            if (isPrime[m]) {
+            if (sieve[m]) {
                 for (int k = m * m; k <= upperBound; k += m)
-                    isPrime[k] = false;
+                    sieve[k] = false;
             }
         }
-        return isPrime;
+        return sieve;
     }
 }
